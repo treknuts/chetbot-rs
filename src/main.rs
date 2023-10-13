ed6f5acf2e199f50d56ed501e97d9a7de2a7dc6f
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-10-13 08:45:37 -0500

    Refactored to use shuttle things for deployment

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use anyhow::anyhow;
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use tracing::info;
+
+#[group]
+#[commands(hello)]
+struct General;
+
+struct Bot;
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
+}
+
+#[command]
+async fn hello(ctx: &Context, msg: &Message) -> CommandResult {
+    info!("{} sent a message in {}", msg.author.name, msg.channel_id);
+    msg.channel_id.say(ctx, "World").await?;
+
+    Ok(())
+}
